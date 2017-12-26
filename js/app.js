81c5288613212f3118efe0fdde9899e07832f7b9
kamalyzl <kamaly.kazal@gmail.com>
2017-12-26 09:45:03 -0500

    Mejorando efecto splash

--- js/app.js
+++ js/app.js
@@ -1,21 +1,69 @@
-function load() {
 
-    var preload = document.getElementById("preload");
-    var loading = 0;
-    var id = setInterval(frame, 40);
 
-    function frame() {
-        if (loading == 100) {
-            clearInterval(id);
-            window.open("views/welcome.html", "_self");
-        }
-        else {
-            loading = loading + 1;
-            if (loading == 90) {
-                preload.style.animation = "fadeout 1s ease";
-            }
-        }
+
+$(window).ready(function () {
+
+    $('#loadPage').delay(2000).fadeOut("slow");
+})
+
+
+$(document).ready(begin);
+
+function begin() {
+    maps();
+    $("#tags").autocomplete({
+        source: availableTags
+    });
+
+}
+
+// Insertando mapa
+var map;
+function initMap() {
+    var mapCenter = new google.maps.LatLng(47.6145, -122.3418); //Google map Coordinates
+    map = new google.maps.Map($("#map")[0], {
+        center: mapCenter,
+        zoom: 10
+    });
+}
+
+function maps() {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            infoWindow = new google.maps.InfoWindow({ map: map });
+            var pos = { lat: position.coords.latitude, lng: position.coords.longitude };
+            infoWindow.setPosition(pos);
+            infoWindow.setContent("Found your location <br />Lat : " + position.coords.latitude + " </br>Lang :" + position.coords.longitude);
+            map.panTo(pos);
+        });
+    } else {
+        console.log("Browser doesn't support geolocation!");
     }
 }
 
-load();
+// Lista de restaurante
+var availableTags = [
+    "Rokys",
+    "Norkys",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure",
+    "COBOL",
+    "ColdFusion",
+    "Erlang",
+    "Fortran",
+    "Groovy",
+    "Haskell",
+    "Java",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Scala",
+    "Scheme"
+];
+
